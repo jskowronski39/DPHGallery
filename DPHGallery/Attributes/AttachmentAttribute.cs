a8cfb3fdd7a9464867858c0433d6edbad38415ea
Jakub Skowroński <jakub.skowronski@movity.pl>
2018-12-04 12:23:12 +0100

    Image upload view model added.

---
+++ DPHGallery/Attributes/AttachmentAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace DPHGallery.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property)]
+	public sealed class AttachmentAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value,
+		  ValidationContext validationContext)
+		{
+			HttpPostedFileBase file = value as HttpPostedFileBase;
+
+			// The maximum allowed file size is 2MB.
+			if (file.ContentLength > 2 * 1024 * 1024)
+			{
+				return new ValidationResult("Rozmiar pliku przekracza 2 MB!");
+			}
+
+			// Only PDF can be uploaded.
+			string ext = Path.GetExtension(file.FileName);
+			if (String.IsNullOrEmpty(ext)
+				|| (!ext.Equals(".jpg", StringComparison.OrdinalIgnoreCase)
+				&& !ext.Equals(".png", StringComparison.OrdinalIgnoreCase))
+			)
+			{
+				return new ValidationResult("Dozwolone formaty plików to: PNG i JPG!");
+			}
+
+			// Everything OK.
+			return ValidationResult.Success;
+		}
+	}
+}
---
+++ DPHGallery/Models/ViewModels/ImageUploadViewModel.cs
@@ -0,0 +1,42 @@
+﻿using DPHGallery.Attributes;
+using DPHGallery.Classes;
+using DPHGallery.ORM.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DPHGallery.Models.ViewModels
+{
+	public class ImageUploadViewModel
+	{
+		/// TODO: Custom validator that validates file format and size
+		[AttachmentAttribute]
+		[Required(ErrorMessage = "Plik jest wymagany!")]
+		public HttpPostedFileBase File { get; set; }
+
+		[StringLength(255, ErrorMessage = "Opis nie może być dłuższy niż 255 znaków!")]
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Creates entity from view model.
+		/// 
+		/// TODO: This should be done by a mapper or some other two way converter (command?)
+		/// </summary>
+		/// <returns>Entity created from model</returns>
+		public ImageEntity ToEntity()
+		{
+			string md5Hash = MD5Utils.MD5ByteHashToString(
+				MD5Utils.GetMD5ByteHashFromStream(this.File.InputStream)
+			);
+
+			return new ImageEntity()
+			{
+				Description = this.Description,
+				ImageHash = md5Hash,
+
+			};
+		}
+	}
+}
