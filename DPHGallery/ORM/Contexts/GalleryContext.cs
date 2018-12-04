d653a28fdc2c7da93c0460a560f5567c6563b1bc
Jakub Skowroński <jakub.skowronski@movity.pl>
2018-12-04 12:05:46 +0100

    Image gallery context added.

---
+++ DPHGallery/ORM/Contexts/GalleryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DPHGallery.ORM.Entities;
+
+namespace DPHGallery.ORM.Contexts
+{
+	public class GalleryContext : DbContext
+	{
+		public GalleryContext() : base("name=DPHGalleryConnectionString")
+		{
+
+		}
+
+		public DbSet<ImageEntity> Images { get; set; }
+	}
+}
