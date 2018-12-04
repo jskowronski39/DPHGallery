91da50128635e3fb303dac3973bc7690badf4959
Jakub Skowroński <jakub.skowronski@movity.pl>
2018-12-04 11:51:20 +0100

    Image entity added.

---
+++ DPHGallery/ORM/Entities/ImageEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DPHGallery.ORM.Entities
+{
+	[Table("Images")]
+	public class ImageEntity
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public Guid Guid { get; set; }
+
+		public Guid Owner { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImagePath { get; set; }
+
+		public string ThumbnailPath { get; set; }
+
+		public string ImageHash { get; set; }
+
+		public int DPH { get; set; }
+	}
+}
