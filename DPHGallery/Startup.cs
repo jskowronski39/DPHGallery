31fbaa8590e3b9b64186bb8087851eedf4e15ecb
Jakub Skowroński <jakub.skowronski@movity.pl>
2018-12-04 23:30:47 +0100

    Added Asp.Net.Identity with default configuration.

---
+++ DPHGallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DPHGallery.Startup))]
+namespace DPHGallery
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+		}
+	}
+}
