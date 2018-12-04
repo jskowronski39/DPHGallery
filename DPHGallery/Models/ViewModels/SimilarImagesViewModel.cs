46f037f0ccc85bdd11fea178836af6da3d46a337
Jakub Skowroński <jakub.skowronski@movity.pl>
2018-12-04 13:06:45 +0100

    Basic implementation of controllers and views.

---
+++ DPHGallery/Models/ViewModels/SimilarImagesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DPHGallery.Attributes;
+using DPHGallery.Classes;
+using DPHGallery.ORM.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DPHGallery.Models.ViewModels
+{
+	public class SimilarImagesViewModel
+	{
+		public ImageEntity SourceImage { get; set; }
+		public IEnumerable<ImageEntity> SimilarImages { get; set; }
+	}
+}
