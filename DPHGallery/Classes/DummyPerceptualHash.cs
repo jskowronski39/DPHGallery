da7a2d7440b666659eea25287814f5c0a346e0f0
Jakub Skowroński <jakub.skowronski@movity.pl>
2018-12-04 12:25:34 +0100

    Added dummy hash computation.

---
+++ DPHGallery/Classes/DummyPerceptualHash.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace DPHGallery.Classes
+{
+	public class DummyPerceptualHash
+	{
+		Image img;
+
+		/// <summary>
+		/// Calculates DPH hash from given image.
+		/// </summary>
+		/// <param name="img">Image</param>
+		public DummyPerceptualHash(Image img)
+		{
+			this.img = img;
+		}
+
+		/// <summary>
+		/// Returns 8-bit image hash as byte.
+		/// </summary>
+		/// <returns>DPH hash</returns>
+		public byte Calculate()
+		{
+			var r = new Random();
+			int hash = r.Next(255);
+
+			return (byte)hash;
+		}
+	}
+}
